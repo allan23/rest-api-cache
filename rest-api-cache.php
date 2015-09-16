548cd988364928a4b39e436aa3ad56a9d88ad061
Allan Collins <allan.collins@10up.com>
2015-09-16 16:24:01 -0600

    Doc updates

--- rest-api-cache.php
+++ rest-api-cache.php
@@ -10,12 +10,20 @@
  */
 add_filter( 'json_pre_dispatch', 'rac_fetch_cache', 10, 2 );
 
+/**
+ * Retrieve or generate cache (if GET).
+ * @param mixed $result Response to replace the requested version with. Can be anything a normal endpoint can return, or null to not hijack the request.
+ * @param WP_JSON_ResponseHandler $server ResponseHandler instance (usually WP_JSON_Server)
+ * @return mixed
+ */
 function rac_fetch_cache( $result, $server ) {
+	// If we're not using a GET method -- let's bail.
 	if ( 'GET' != $server->method ) {
 		return $result;
 	}
 	$cache_key	 = md5( $_SERVER[ 'REQUEST_URI' ] );
 	$result		 = wp_cache_get( $cache_key, 'rac' );
+	// Check for the 'cache_bust=1' query string.
 	$cache_bust	 = filter_input( INPUT_GET, 'cache_bust', FILTER_VALIDATE_BOOLEAN );
 	if ( false == $result || $cache_bust ) {
 		$result = $server->dispatch();
